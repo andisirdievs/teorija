dec7ace4484aacf9dfd5c5e8e3dd7aeb2428d5bb
lietotajs <lietotajs@121-03>
2019-12-20 10:56:35 +0200

    master

---
+++ src/lv/jak/bujaks/sintakse/Sintakse.java
@@ -0,0 +1,66 @@
+package lv.jak.bujaks.sintakse;//Klases atrasšanās vieta projektā
+								/*vairaku
+								 * rindu
+								 * komentars
+								 */
+//import komanda kas nodrošina citas klases  vai pakotnes piesaisti šai klasei.
+//Tādā veidā tiek nodrošinātas dažadu funkciju izmantošanas iespēju šajā klasē, piemēram datu izvade no tastatūras  
+// Rezervētie vārdi abstract	assert	boolean	break
+/*byte	case	catch	char
+class	const	continue	default
+do	double	else	enum
+extends	final	finally	float
+for	goto	if	implements
+import	instanceof	int	interface
+long	native	new	package
+private	protected	public	return
+short	static	strictfp	super
+switch	synchronized	this	throw
+throws	transient	try	void
+volatile	while*/
+//import <pakotne> . <Klase>;
+//import <pakotne>. *;
+import java.util.Scanner;
+//kLASES NOSAUKUMS
+		/*1.Klases nosaukums sākas ar lielo burtu
+		 *2.Ja klases nosaukums satur vairaks vardus, katrs vārds ar lielo burtu
+		 *3.Nosaukums var saturet tikai burtus un ciparus, neko citu 
+		 *4.Ja klases nosaukums satur vairākus vārdus ar atstarpēm tos neatdala
+		 *
+		 * 
+		 */
+//Javas klases dala - galvenā klases daļa
+
+public class Sintakse { //Klases ķermenis
+	//Šajā daļā koda fragments ir pieejams visai klasei
+	//Izņēmuma nosacījums - aprakstītās lietas ir redzamas tikai tai daļai koda, kas atrodas zemāk
+
+	//metoda MAIN - galvenā metode
+	/*Main metode
+	 * 	1.Uz projektu jābūt vismaz vienai main metodei
+	 * 	2.Metodes pieraksts ir tieši  tāds kā redzams zemāk pierakstā
+	 */
+	//!!!! NEDRĪKST RAKSTĪT FUNKCIONĀLUS PROCESUS! IZŅEMOT VĒRTĪBAS PIEŠĶIRŠANU VAI OBJEKTA IZVEIDI
+	//MAINĪGIE - PROGRAMMĒŠANAS VALODAS PAMATELEMENTS
+	//sINTAKSES NOSACĪJUMI UZ MAINĪGA NOSAUKUMU
+	/*	1.MAINĪGĀ NOSAUKUMS SĀKAS AR MAZO BURTU
+	 * 	2.JA MAINĪGĀ NOSAUKUMS SATUR VAIRĀKUS VĀRDUS, KATRS NĀKAMAIS SĀKAS AR LIELO BURTU
+	 * 	3.Ja MAINĪGĀ nosaukums satur vairākus vārdus ar atstarpēm tos neatdala
+	 * 	4.VAR IZMANTOT TIKAI BURTUS UN CIPARUS, BET IZŅĒMUMI-
+	 * IZŅĒMUMI:
+	 *
+	 */
+	//Mainīgā izveidošana
+	//Pieraksta forma:
+	//		---> ja iepriekš anv izveidots: <datu tips> <mainiga nosauums>
+	int veselsSkaitlis;
+	
+	
+	
+	
+	public static void main(String[] args) { //Ķermenis
+		//metodes ķermenis - aprakstam algoritmu, kuru vēlamies izpildīt
+	
+		}
+	}//Klases ķermenis ir beidzies
+//NEKO ŠEIT NEDRĪKST RAKSTĪT!!!!!
